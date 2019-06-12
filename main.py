4823c6973f9354bbe22ea7be2f74b4a3218f7880
1123581 <47376876+1123581@users.noreply.github.com>
2019-06-12 10:30:55 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import logging
+from github import Github
+
+path = "/home/amare/Documents/GitHub/"
+
+username = "" #GitHub usr name
+password = "" #GitHub password
+
+def main():
+    folderName = "newrepo"
+    os.makedirs(path + folderName)
+    user = Github(username, password).get_user()
+    repo = user.create_repo(folderName)
+    print("Succesfully created repository")
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+    main()
